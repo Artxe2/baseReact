a6e9beb6c9e79ebcdb4404c7a9d184166b440249
artxe <balgoon24@gmail.com>
2020-12-09 16:11:45 +0900

    asd

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,41 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    isLoading: true,
-    groups: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch('/base/current_time');
-    const body = await response.json();
-    this.setState({ res: body, isLoading: false });
-  }
-
   render() {
-    const {res, isLoading} = this.state;
-
-    if (isLoading) {
-      return (
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-intro">
-            <h2>Response</h2>
-            <p>Loading...</p>
-          </div>
-        </header>
-      </div>
-      )
-    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-intro">
-            <h2>Response</h2>
-            {res}
-          </div>
+          <p>
+            Hello React Project!
+          </p>
         </header>
       </div>
     );

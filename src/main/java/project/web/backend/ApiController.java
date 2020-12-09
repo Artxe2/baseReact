d2d2aab695836f23c2fad96aafa3937c14a9b7a3
artxe <balgoon24@gmail.com>
2020-12-09 18:45:49 +0900

    Get Customers REST API Module

--- src/main/java/project/web/backend/ApiController.java
+++ src/main/java/project/web/backend/ApiController.java
@@ -17,4 +17,44 @@ public class ApiController {
 		logger.info("ApiController - hello");
 		return "{\"message\": \"Hello Express!\"}";
 	}
+
+	@GetMapping("customers")
+	public Object customers(@RequestParam Map<String, Object> pMap) {
+		logger.info("ApiController - customers");
+		return 
+			"[" + 
+			"  {" + 
+			"    \"id\": 1," + 
+			"    \"image\": \"https://placeimg.com/64/64/1\"," + 
+			"    \"name\": \"나동빈\"," + 
+			"    \"birthday\": \"961222\"," + 
+			"    \"gender\": \"남자\"," + 
+			"    \"job\": \"대학생\"" + 
+			"  }," + 
+			"  {" + 
+			"    \"id\": 2," + 
+			"    \"image\": \"https://placeimg.com/64/64/2\"," + 
+			"    \"name\": \"홍진호\"," + 
+			"    \"birthday\": \"222222\"," + 
+			"    \"gender\": \"남자\"," + 
+			"    \"job\": \"프로게이머\"" + 
+			"  }," + 
+			"  {" + 
+			"    \"id\": 3," + 
+			"    \"image\": \"https://placeimg.com/64/64/3\"," + 
+			"    \"name\": \"이순신\"," + 
+			"    \"birthday\": \"921205\"," + 
+			"    \"gender\": \"남자\"," + 
+			"    \"job\": \"디자이너\"" + 
+			"  }," + 
+			"  {" + 
+			"    \"id\": 4," + 
+			"    \"image\": \"https://placeimg.com/64/64/4\"," + 
+			"    \"name\": \"홍길동\"," + 
+			"    \"birthday\": \"960305\"," + 
+			"    \"gender\": \"남자\"," + 
+			"    \"job\": \"프로그래머\"" + 
+			"  }" + 
+			"]";
+	}
 }
